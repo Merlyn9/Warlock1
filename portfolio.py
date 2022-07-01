cdd9f16ec231eb6cf794e36b5fdc1f344b55f9c3
Jason Mills <roninmerlyn@gmail.com>
2022-07-01 08:37:45 -0500

    First Python File
    First Python file

---
+++ portfolio.py
@@ -0,0 +1,110 @@
+import webbrowser
+
+#Opens up Web browser
+#webbrowser.open('https://example.com/')
+
+print("Welcome,  I am looking to work for your company. Hit Enter")
+input('')
+
+print("This is a python prgram to highlight all of my profiles so you as a potential employer can see what I have been up to")
+input('')
+
+print("We can start with the Kanban Flow.")
+input('')
+
+print("Kanban simply is a method by which one can consolidate all projects.")
+input('')
+
+print('Kanban your life!!!  Kanban 4 Life!!!!')
+webbrowser.open('https://www.kanbanflow.com/', new=2)
+input('')
+
+print("Microsoft")
+webbrowser.open('https://account.microsoft.com/', new=2)
+print("Don't collect too many windows.")
+input('')
+
+print("Gmail")
+webbrowser.open('https://mail.google.com/')
+input('')
+
+print("picoCTF")
+webbrowser.open('https://play.picoctf.org/login', new=2)
+print("Stay Sharp")
+input('')
+
+print("Python Institute Free Education")
+webbrowser.open('https://www.pythoninstitute.org/', new=2)
+print("Free online study. Great course.  Get certifed.")
+input('')
+
+
+print("Github")
+webbrowser.open('https://github.com/')
+input('')
+
+print("Docker")
+webbrowser.open('https://docker.com/')
+input('')
+
+print("Amazon AWS")
+webbrowser.open('https://aws.amazon.com/')
+input('')
+
+print("Linode")
+webbrowser.open('https://www.linode.com/')
+input('')
+
+print("Mturk")
+webbrowser.open('https://www.mturk.com/')
+input('')
+
+print("Redbubble")
+input('')
+
+print("Anchor.fm")
+input('')
+
+print("Hashnode")
+input('')
+
+print("Banking")
+input('')
+
+print("Paypal")
+input('')
+
+print("CashApp")
+input('')
+
+print("Blockfi")
+input('')
+
+
+print("Circle CI")
+webbrowser.open('https://circleci.com/')
+input('')
+
+
+print("Mongo DB")
+webbrowser.open('https://www.mongodb.com/')
+input('')
+
+print("")
+webbrowser.open('https://www.mongodb.com/')
+input('')
+
+print("")
+input('')
+
+print("")
+input('')
+
+print("")
+input('')
+
+print("")
+input('')
+
+print("")
+input('')
